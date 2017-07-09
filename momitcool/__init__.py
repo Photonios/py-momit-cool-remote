a25e18edfae3a85c173194d861e57fd0b1dae127
Swen Kooij <swenkooij@gmail.com>
2017-07-09 19:49:06 +0300

    Turn off after 24 hours tops

--- momitcool/__init__.py
+++ momitcool/__init__.py
@@ -14,17 +14,39 @@ class MomitCool:
     def on(self):
         """Turns on the air-conditioning."""
 
+        params = (
+            ('cm', 'cool'), # mode: 'cool' or 'heat'
+            ('t', '1440'), # time till turn off in minutes, 24 hours
+            ('sp', '0'), # target temperature, 290 = 29.0C
+        )
+
         message = CoapMessage(host=self.host, port=self.port)
         message.options.path = ['10242', '0', '0']
         message.options.content_format = 1541
-        message.payload = b'cm=cool,t=472,sp=200'
+        message.payload = self._encode_parameters(params)
         message.send()
 
     def off(self):
         """Turns off the air-conditioning."""
 
+        params = (
+            ('cm', 'cool')
+            ('t', '0'),
+            ('sp', 'off')
+        )
+
         message = CoapMessage(host=self.host, port=self.port)
         message.options.path = ['10242', '0', '0']
         message.options.content_format = 1541
-        message.payload = b'cm=cool,t=0,sp=off'
+        message.payload = self._encode_parameters(params)
         message.send()
+
+    def _encode_parameters(self, parameters: dict):
+        """Encodes the payload parameters for transmission."""
+
+        result = ','.join([
+            '%s=%s' % (key, value)
+            for key, value in parameters
+        ])
+
+        return result.encode()
